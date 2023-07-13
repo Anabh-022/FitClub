97a6adcd73882989a7cf638a06099e2f19e868fe
Ayush Pandey <ayushji3sep@gamil.com>
2023-07-13 22:24:28 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
 import './App.css';
+import Hero from './Components/Hero/Hero'
+import PLans from './Components/Plans/PLans';
+import Program from './Components/Programs/Program';
+import Reasons from './Components/Reasons/Reasons';
+import Testimonial from './Components/Testimonial/Testimonial';
+
+import Join from './Components/Join/Join';
+import Footer from './Components/Footer/Footer';
 function App() {
   return (
     <div className="App">
-          <span style={{color: 'white'}}>Subscribe Zainkeepscode</span>
+      <Hero/>
+      <Program/>
+      <Reasons/>
+      <PLans/>
+      <Testimonial/>
+      <Join/>
+      <Footer/>
     </div>
   );
 }
